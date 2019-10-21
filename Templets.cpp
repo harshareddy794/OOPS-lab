523c010834d3a3753d8e283a04af008a2ae99627
Gaddam Harshavardhan <48166328+harshareddy794@users.noreply.github.com>
2019-10-21 22:01:45 +0530

    Create Templets.cpp

---
+++ Templets.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+template<class T>
+T sum(T a,T b)
+{
+return(a+b);
+}
+int main()
+{
+clrscr();
+int a=10,b=20,c;
+long k=11,l=22,m;
+c=sum(a,b);
+cout<<"Sum of integers" <<c;
+m=k+l;
+cout<<"Sum of long values"<<m;
+getch();
+return 0;
+}
