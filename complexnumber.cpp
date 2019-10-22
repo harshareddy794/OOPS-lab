2231d0b7a26e0956f0bd7840f5010ac23872cc6c
Gaddam Harshavardhan <48166328+harshareddy794@users.noreply.github.com>
2019-10-22 07:49:31 +0530

    Create complexnumber.cpp

---
+++ complexnumber.cpp
@@ -0,0 +1,33 @@
+#include<iostream.h>
+#include<conio.h>
+class A
+{public:
+int img,rel;
+void get()
+{
+cout<<"Enter imaginary part";
+cin>>img;
+cout<<"Enter real part";
+cin>>rel;
+}
+A operator + (A ob)
+{ A obj;
+obj.img=img+ob.img;
+obj.rel=rel+ob.rel;
+return(obj);
+}
+void dis()
+{
+cout<<rel<<"i"<<img<<endl;
+}
+};
+void main()
+{
+clrscr();
+A ob1,ob2,res;
+ob1.get();
+ob2.get();
+res=ob1+ob2;
+res.dis();
+getch();
+}
